84a69bb06d608851cd9f0f2d3d774d32bf2cf1e2
Ramana-Muthyala <ramana_muthyala@yahoo.co.in>
2022-12-15 22:51:14 +0530

    Minor change.

--- src/main/java/ramana/example/niotcpserver/codec/http/request/RequestCodec.java
+++ src/main/java/ramana/example/niotcpserver/codec/http/request/RequestCodec.java
@@ -6,7 +6,6 @@ import ramana.example.niotcpserver.io.Allocator;
 import ramana.example.niotcpserver.types.InternalException;
 
 import java.nio.ByteBuffer;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -18,7 +17,6 @@ import java.util.Queue;
  */
 public class RequestCodec {
     private AbstractParser<RequestMessage> parser;
-    private final Deque<Allocator.Resource<ByteBuffer>> dataDeque = new LinkedList<>();
     private final Queue<RequestMessage> messageQueue = new LinkedList<>();
 
     public RequestCodec() {
@@ -28,15 +26,12 @@ public class RequestCodec {
     public void decode(Allocator.Resource<ByteBuffer> data) throws InternalException, ParseException {
         ByteBuffer byteBuffer = data.get();
         byteBuffer.flip();
-        dataDeque.offer(data);
-        while((data = dataDeque.poll()) != null) {
-            byteBuffer = data.get();
+        while(byteBuffer.hasRemaining()) {
             parser.parse(data);
             if(parser.getStatus() == AbstractParser.Status.DONE) {
                 messageQueue.offer(parser.getResult());
                 parser = new RequestParser();
             }
-            if(byteBuffer.hasRemaining()) dataDeque.offerFirst(data);
         }
     }
 
