65856366843d97fbed22313a8ebf83caafe39456
Ramana-Muthyala <ramana_muthyala@yahoo.co.in>
2022-12-16 21:28:35 +0530

    Http codec tested.

--- src/test/java/ramana/example/niotcpserver/example/codec/http/LatencyTestHttpClient.java
+++ src/test/java/ramana/example/niotcpserver/example/codec/http/LatencyTestHttpClient.java
@@ -11,39 +11,51 @@ import ramana.example.niotcpserver.util.Util;
 import java.nio.ByteBuffer;
 import java.util.logging.Logger;
 
-public class EchoHttpClientMultipleRequests {
+public class LatencyTestHttpClient {
     private static final Logger logger = LogFactory.getLogger();
 
     public static void main(String[] args) {
         new Client().connect("localhost", 8080)
                 .enableDefaultRead()
-                .enableLogging()
                 .channelHandler(new ChannelHandler())
                 .run();
     }
 
     public static class ChannelHandler extends ChannelHandlerAdapter {
+        private long start;
+
         @Override
         public void onConnect(Context.OnConnect context, Object data) throws InternalException {
-            for (int i = 0; i < 3; i++) {
-                Allocator.Resource<ByteBuffer> resource = context.allocator().allocate(1024);
-                ByteBuffer buffer = resource.get();
-                String request = "GET /index.html?name=ramana HTTP/1.1\r\n" +
-                        "Host:localhost\r\n" +
-                        "User-Agent:NIOTCPServer/1.0\r\n" +
-                        "Accept:*.*\r\n" +
-                        "Accept-Encoding:gzip, deflate, br\r\n" +
-                        "Connection:keep-alive\r\n" +
-                        "\r\n";
-                buffer.put(request.getBytes());
-                context.write(resource);
-                context.flush();
-            }
+            start = System.nanoTime();
+            write0(context);
+        }
+
+        private void write0(Context.OnRead context) throws InternalException {
+            Allocator.Resource<ByteBuffer> resource = context.allocator().allocate(1024);
+            ByteBuffer buffer = resource.get();
+            String request = "GET / HTTP/1.1\r\n" +
+                    "Host:localhost\r\n" +
+                    "User-Agent:NIOTCPServer/1.0\r\n" +
+                    "Accept:*.*\r\n" +
+                    "Accept-Encoding:gzip, deflate, br\r\n" +
+                    "Connection:keep-alive\r\n" +
+                    "\r\n";
+            buffer.put(request.getBytes());
+            context.write(resource);
+            context.flush();
         }
 
         @Override
         public void onRead(Context.OnRead context, Object data) throws InternalException {
-            logger.info("Received: \n" + Util.toString(data));
+            counter++;
+            if(counter == 100000) {
+                logger.info("Time taken: (ms): " + ((double)(System.nanoTime() - start) / 1000000));
+                context.close();
+                return;
+            }
+            write0(context);
         }
+
+        private int counter;
     }
 }
---
+++ src/test/java/ramana/example/niotcpserver/example/codec/http/LatencyTestHttpServer.java
@@ -0,0 +1,42 @@
+package ramana.example.niotcpserver.example.codec.http;
+
+import ramana.example.niotcpserver.Bootstrap;
+import ramana.example.niotcpserver.codec.http.Processor;
+import ramana.example.niotcpserver.codec.http.RequestResponseHolder;
+import ramana.example.niotcpserver.codec.http.Util;
+import ramana.example.niotcpserver.codec.http.handler.CodecChannelHandler;
+import ramana.example.niotcpserver.codec.http.handler.ProcessorChannelHandler;
+import ramana.example.niotcpserver.codec.http.request.Field;
+import ramana.example.niotcpserver.codec.http.response.ResponseMessage;
+
+import java.util.ArrayList;
+
+public class LatencyTestHttpServer {
+
+    public static void main(String[] args) {
+        new Bootstrap().listen(8080)
+                .enableDefaultRead()
+                .channelHandler(CodecChannelHandler.class)
+                .channelHandler(ChannelHandler.class)
+                .start();
+    }
+
+    public static class ChannelHandler extends ProcessorChannelHandler {
+        @Override
+        protected Processor create() {
+            return new EchoProcessor();
+        }
+    }
+
+    public static class EchoProcessor implements Processor {
+        @Override
+        public void process(RequestResponseHolder requestResponseHolder) {
+            ResponseMessage responseMessage = requestResponseHolder.responseMessage;
+            responseMessage.statusCode = Util.STATUS_OK;
+            responseMessage.body = "Hello World !!".getBytes();
+            ArrayList<String> values = new ArrayList<>(1);
+            values.add(String.valueOf(responseMessage.body.length));
+            responseMessage.headers.add(new Field(Util.REQ_HEADER_CONTENT_LENGTH, values));
+        }
+    }
+}
